dd1e9f36fde10f08b6332e7d3e0558a80d1e6ea9
Jari Maijenburg <jari.maijenburg@ivido.nl>
2022-12-02 22:56:45 +0100

    Rewrote reading function

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -54,28 +54,34 @@ impl Hand {
 }
 
 fn part1() -> i32 {
-    let mut score = 0;
-
-    for line_res in read_lines("input.txt") {
-        match line_res {
-            Ok(line) => {
-                let hands = line
-                    .split_whitespace()
-                    .map(|c| c.chars().next().unwrap())
-                    .map(|c| Hand::from_char(&c))
-                    .collect::<Vec<Hand>>();
-
-                score += hands[1].play(&hands[0]);
-            }
-            Err(error) => panic!("Problem reading line: {:?}", error),
-        };
-    }
-
-    score
+    read_games("input.txt")
+        .iter()
+        .map(|(theirs, ours)| ours.play(theirs))
+        .sum()
 }
 
 fn main() {
     println!("Part 1: The score is {}", part1());
+}
+
+fn read_games<P>(filename: P) -> Vec<(Hand, Hand)>
+where
+    P: AsRef<Path>,
+{
+    read_lines(filename)
+        .map(|line| line.unwrap())
+        .map(|line| {
+            line.split_once(" ")
+                .map(|(c1, c2)| (c1.to_owned(), c2.to_owned()))
+                .unwrap()
+        })
+        .map(|hand_strs| {
+            (
+                Hand::from_char(&hand_strs.0.chars().next().unwrap()),
+                Hand::from_char(&hand_strs.1.chars().next().unwrap()),
+            )
+        })
+        .collect()
 }
 
 fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
